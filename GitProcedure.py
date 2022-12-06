50b3fc3ed73a6814dfaa603baf2edfb9f47d282d
DanielQu1108 <daniel.qu@mclaren.com>
2022-12-06 17:10:45 +0000

    Added git instruction

---
+++ GitProcedure.py
@@ -0,0 +1,26 @@
+"""
+For setting up a new Git Repo:
+
+    git init
+    git add -A
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/DanielQu1108/UploadSample.git      Substitute with git link
+    git push -u origin main
+
+
+For future changes:
+
+    git add -A      //      git add .
+    !! USE git status TO CHECK STATUS BEFORE COMMIT, SHOULD BE GREEN!!
+    git commit -m "first commit"        Substitute with commit comment
+    git push
+
+When it asks for username and password:
+
+    Usename: daniel.qu@mclaren.com
+    Password: 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
+
+Same on Rock Pi and vscode
+
+"""
